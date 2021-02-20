16c3b446018cc33b6d7aa771ca0ae988fea06d2f
Rosa Pena <rosa.pena51@myhunter.cuny.edu>
2021-02-19 22:42:24 -0500

    CodePath: Flix - Part 2 This section includes Detailed View, tab bar button, and grid layout

---
+++ flixter/MovieGridViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridViewCell.swift
+//  flixter
+//
+//  Created by Rosa Peña on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
